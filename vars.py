ede8c614995ac905220621e755ace4338eae5bb8
vishal009828 <vishalkumar76682275@gmail.com>
2025-07-08 15:42:31 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,14 +1,14 @@
-#🇳‌🇮‌🇰‌🇭‌🇮‌🇱‌
+#Bobby
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "xxxxxxxxx"))
-API_HASH = environ.get("API_HASH", "xxxxxxxxxxxxxxxxxxxxxxx")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5680454765"))
-CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+API_ID = int(environ.get("API_ID", "21241424"))
+API_HASH = environ.get("API_HASH", "e8318a4df05644acdf6705e1d67280f8")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7811042291:AAHQdxQHvnjUPcmVfUmo43mijyzllJv6bVM")
+OWNER = int(environ.get("OWNER", "7323329628"))
+CREDIT = environ.get("CREDIT", "@KingXHD")
+AUTH_USER = os.environ.get('AUTH_USERS', '7323329628').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
